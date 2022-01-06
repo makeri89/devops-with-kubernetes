2a5ee3e07dbe1912c3590839a1867ee7fafa3dff
Markus Kaihola <markus.kaihola@helsinki.fi>
2022-01-06 19:03:17 +0200

    started ex 1.10

--- pingpong/index.js
+++ pingpong/index.js
@@ -1,11 +1,25 @@
 const express = require('express')
+const fs = require('fs')
+const path = require('path')
 
 const app = express()
 
+const directory = path.join('/', 'usr', 'src', 'app', 'logs')
+const filePath = path.join(directory, 'pongs.txt')
+
 let count = 0
+
+const saveCount = (count) => {
+  fs.writeFile(filePath, String(count), (error) => {
+    if (error) {
+      console.log(error)
+    }
+  })
+}
 
 app.get('/', (_req, res) => {
   res.send(`pong ${count}`)
+  saveCount(count)
   count++
 })
 
