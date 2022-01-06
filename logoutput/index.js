05e0e950e84e82498dbdaefca2978fddb8ab8099
Markus Kaihola <markus.kaihola@helsinki.fi>
2022-01-06 14:48:14 +0200

    ex 1.07 & 1.08, 1.09 not working yet

--- logoutput/index.js
+++ logoutput/index.js
@@ -1,17 +1,27 @@
 const moment = require('moment')
 const { v4: uuidv4 } = require('uuid')
+const express = require('express')
+
+const app = express()
+
+const randomString = uuidv4()
+
+app.get('/status', (_req, res) => {
+  res.send(getTimestampedString(randomString))
+})
+
+const getTimestampedString = (string) => {
+  const timestamp = moment().format()
+  return `${timestamp}: ${string}`
+}
 
 const logger = (string) => {
-  const timestamp = moment().format()
-
-  console.log(`${timestamp}: ${string}`)
-
+  console.log(getTimestampedString(string))
   setTimeout(logger, 5000, string)
 }
 
-const main = () => {
-  const randomString = uuidv4()
+const PORT = 3000
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`)
   logger(randomString)
-}
-
-main()
+})
---
+++ pingpong/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+
+const app = express()
+
+let count = 0
+
+app.get('/pingpong', (_req, res) => {
+  res.send(`pong ${count}`)
+  count++
+})
+
+const PORT = 4000
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`)
+})
